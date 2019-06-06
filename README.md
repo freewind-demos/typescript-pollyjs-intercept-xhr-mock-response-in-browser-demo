257690fa474e4bf2ad69e2758969cdabe0f55be1
Freewind <nowindlee@gmail.com>
2019-06-06 17:35:14 +0800

    demo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-TypeScript Webpack Demo
-=======================================
+TypeScript Pollyjs Intercept "fetch" and Mock Response in Browser Demo
+=======================================================================
+
+Use Pollyjs to intercept `window.fetch` and return mock response in browser.
 
 ```
 npm install
--- entry.ts
+++ entry.ts
@@ -1,4 +1,40 @@
-import $ from 'jquery'
-import './index.css';
+import {Polly} from '@pollyjs/core';
+import FetchAdapter from '@pollyjs/adapter-fetch';
 
-$('#main').text('Hello, webpack!');
+Polly.register(FetchAdapter);
+
+const url = 'https://jsonplaceholder.typicode.com/posts/404';
+
+function setupServer() {
+  const polly = new Polly('Simple Client-Side Server Example', {
+    adapters: ['fetch'], // Hook into `fetch`
+    logging: true // Log requests to console
+  });
+  const {server} = polly;
+
+  server.get(url).intercept((req, res) => {
+    res.status(200).json({content: 'Hello, Pollyjs!'});
+  });
+}
+
+async function fetchData() {
+  const response = await fetch(url);
+  return await response.json();
+}
+
+function setupButton() {
+  const button = document.getElementById('button')!;
+  const message = document.getElementById('message')!;
+
+  button.addEventListener('click', async () => {
+    const post = await fetchData();
+    message.innerText = JSON.stringify(post)
+  })
+}
+
+function run() {
+  setupServer();
+  setupButton();
+}
+
+run();
